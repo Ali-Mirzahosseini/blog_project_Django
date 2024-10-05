42ddf794f62c68439d3161a942e7fe63f64c503e
Ali-Mirzahosseini <ali.mirzahosseini.dev@gmail.com>
2024-10-05 10:04:04 +0330

    Commit #2

---
+++ django_project/__init__.py
@@ -0,0 +1,8 @@
+# def oddOrEven(number):
+#     if number % 2 == 0:
+#         print(f'number {number} is even')
+#     else:
+#         print(f'number {number} is odd')
+#
+# while True:
+#     oddOrEven(int(input('Enter a number: ')))
